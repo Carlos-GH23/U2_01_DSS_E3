f9bec0409c46c24d579357dc6913e28eee4bf8dc
PWM-Alex <VictorAlex1233@hotmail.com>
2025-02-23 16:11:06 -0600

    Vista Personas
    Modulo de la vista Personas

---
+++ Front-End/Practicas/src/app/persons/persons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-persons',
+  imports: [NavbarComponent],
+  templateUrl: './persons.component.html',
+  styleUrl: './persons.component.css'
+})
+export class PersonsComponent {
+
+}
